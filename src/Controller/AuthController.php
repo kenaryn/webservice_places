4c724a8deaf1cb130071e9bd4ecdbc545300fb2c
aurele <aurelien.plazzotta@tutanota.com>
2024-06-19 11:15:47 +0200

    Added Auth controller and edit security.yaml to enforce security user firefalls pattern and app user provider with username as id

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PassowrdHasher\Hasher\UserPasswordHaserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthController extends ApiController
+{
+   public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+   {
+      $request = $this-transformJsonBody($request);
+      $username = $request->get('username');
+      $password = $request->get('password');
+      if (empty($username) || empty($password)) {
+         return $this->respondValidationError('Invalid username or pass word.');
+      }
+
+      $user = new User($username);
+      $user->setPassword($passwordHasher->hashPassword(
+         $user,
+         $password
+      ));
+      $user->setUsername($username);
+      $em->persist($user);
+      $em->flush();
+
+      return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
+   }
+
+   /**
+    * @param UserInterface $user
+    * @param JWTTokenManagerInterface $JWTManager
+    * @param JsonResponse
+    */
+   public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+   {
+      return new JsonResponse(['token' => $JWTManager->create($user)]);   
+   }
+}
+?>
